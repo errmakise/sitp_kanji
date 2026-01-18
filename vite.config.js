82d45576a8fe30553d5e4ef7fe049c447610409b
errmakise <120027154+errmakise@users.noreply.github.com>
2026-01-18 15:06:47 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sitp-kanji/',
+  base: '/sitp_kanji/',
   plugins: [
     vue(),
     vueDevTools(),
